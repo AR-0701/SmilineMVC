0dfc837e090604b814f065e8d156dc51a2895b5a
GARCIA MENDOZA ALEX ROLANDO <213107157@cuautitlan.tecnm.mx>
2025-06-19 08:56:43 -0600

    modificacion error

--- modelo/modeloFiltrarCitas.php
+++ modelo/modeloFiltrarCitas.php
@@ -52,24 +52,11 @@ function filtrarCitasPorFecha($fecha)
                 echo "<td>" . htmlspecialchars($fila['dia']) . "</td>";
                 echo "<td>" . htmlspecialchars($fila['hora']) . "</td>";
 
-<<<<<<< HEAD
                 echo "<td>
                                         <button class='action-btn btn-schedule' data-bs-toggle='modal' data-bs-target='#historialModal' title='Agregar Historial'>
                                             <i class='fa-solid fa-file-medical'></i>
                                         </button>
                                     </td>";
-
-
-=======
-                if ($fila['tipo_cita'] == 'temporal') {
-                    echo "<td>
-            <button class='button1' onclick='eliminarCitaTemporal(" . htmlspecialchars($fila['idCita']) . ")'>Eliminar</button>
-          </td>";
-                } else {
-                 
-                }
->>>>>>> 16e36cc0eeb5924bba12addb4473b8da315631c2
-
                 echo "</tr>";
             }
         } else {
