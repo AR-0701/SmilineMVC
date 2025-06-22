7012dff31d21e081be262f2d075da12535101c0b
GARCIA MENDOZA ALEX ROLANDO <213107157@cuautitlan.tecnm.mx>
2025-06-22 17:58:25 -0600

    correccion foo

--- vistas/agendar.view.php
+++ vistas/agendar.view.php
@@ -25,7 +25,7 @@ $clienteLogueado = [
     body {
         background: linear-gradient(to top, #13cdbd, #5a18ff);
         padding-top: 30px;
-          min-height: 100vh;
+        min-height: 100vh;
         margin: 0;
     }
 
