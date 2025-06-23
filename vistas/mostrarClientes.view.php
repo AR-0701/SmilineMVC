d8a25a1d2778b83ba7ac4d3b2d82117edb027bf2
Rivero Rivero <AdanaeRivero>
2025-06-22 19:18:26 -0600

    1

--- vistas/mostrarClientes.view.php
+++ vistas/mostrarClientes.view.php
@@ -467,7 +467,7 @@ include '../controladores/controladorVerClientes.php';
                     <span class="sidebar-menu-text">Inicio</span>
                 </a>
 
-                <a href="../public/mostrarClientes.php" class="sidebar-menu-item">
+                <a href="../public/mostrarClientes.php" class="sidebar-menu-item active">
                     <svg class="sidebar-menu-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -476,7 +476,7 @@ include '../controladores/controladorVerClientes.php';
                     <span class="sidebar-menu-text">Pacientes</span>
                 </a>
 
-                <a href="../public/mCitas.php" class="sidebar-menu-item active">
+                <a href="../public/mCitas.php" class="sidebar-menu-item">
                     <svg class="sidebar-menu-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
